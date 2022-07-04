0f339e07c297171a366c6b643ad11bd653010c89
Khienna <saishat645@gmail.com>
2022-07-04 11:54:02 -0700

    Task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int d;
+	int e;
+	int f;
+
+	for (d = 48 ; d <= 57 ; d++)
+	{
+		for (e = 49 ; d <= 57 ; e++)
+		{
+			for (f = 50 ; f <= 57 ; f++)
+			{
+				if (f > e && e > d)
+				{
+				putchar(d);
+				putchar(e);
+				putchar(f);
+				if (d != 55 || e != 56)
+				{
+					putchar(44);
+					putchar(32);
+				}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
