0d99d6c7649f1e39d796330dc19f8aebeb4767ce
Khienna <saishat645@gmail.com>
2022-07-05 10:10:45 -0700

    pre

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c[8] = "_putchar";
+	int i;
+
+	for (i = 0 ; i <= 7 ; i++)
+	{
+		_putchar(c[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return - Always 0 (Success)
+ */
+int main(void)
+{
+	char _putchar (char c);
+return (0);
+}
