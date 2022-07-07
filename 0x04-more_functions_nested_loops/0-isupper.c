7cccad4e867e52f6a91b2eec9f41cb708b90dff4
Khienna <saishat645@gmail.com>
2022-07-07 04:00:39 -0700

    Tasks

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: character to be checked
+ * Return: 1
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && C <= 90)
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - multiplies two integers
+ * @a: to be multiplied by b
+ * @b: to be multiplied by a
+ * Return: 0.
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	printf("%d\n", c);
+	return (0);
+}
