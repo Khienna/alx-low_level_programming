4006b06bdb3b6d1eeb3b8dd8b27f728a2eb0ec37
Khienna <saishat645@gmail.com>
2022-07-08 15:29:17 -0700

    8..

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,6 +9,10 @@ void print_square(int size)
 {
 	int a, b;
 
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
 	for (a = 0 ; a < size ; a++)
 	{
 		for (b = 0 ; b < size ; b++)
@@ -17,5 +21,5 @@ void print_square(int size)
 		}
 		_putchar('\n');
 	}
-	_putchar('\n');
+	}
 }
