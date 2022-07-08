acb9436b9d6c0f8a1b0316fa951849c9e4b1a271
Khienna <saishat645@gmail.com>
2022-07-08 16:20:14 -0700

    10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,4 +7,26 @@
  */
 void print_triangle(int size)
 {
+	int a - 1, b;
 
+	while (a <= size && size > 0)
+	{
+		b = 0;
+		while (b < size - a)
+		{
+			_putchar(' ');
+			b++;
+		}
+		b = 0;
+		while (b < a)
+		{
+			_putchar('#');
+			b++;
+		}
+
+		_putchar('\n');
+		a++;
+	}
+	if (a == 1)
+		_putchar('\n');
+}
