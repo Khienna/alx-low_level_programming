4dd882fc62d905534dc9c246534cc20d88494ed4
Khienna <saishat645@gmail.com>
2022-07-12 14:02:36 -0700

    task10

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _atoi - changes character to int
+ * @s: character to be changed
+ * Return: int variable
+ */
+int _atoi(char *s)
+{
+	int a;
+
+	a = atoi(s);
+	return (a);
+}
