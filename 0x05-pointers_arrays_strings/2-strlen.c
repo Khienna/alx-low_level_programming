9919abc08d76c679280fbee8ffd79755fb8b9f37
Khienna <saishat645@gmail.com>
2022-07-12 08:31:30 -0700

    task2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strlen - length of string
+ * @s: string
+ * Return: void
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(s);
+	return (len);
+}
