74b2d4d9971e229111e2d893d2bcf48740612d4f
Khienna <saishat645@gmail.com>
2022-07-12 12:26:02 -0700

    task6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts2- puts even arrays
+ * @str: array
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int a, b;
+
+	a = strlen(str);
+	for (b = a - 1; b >= 0 ; b--)
+	{
+		if (b % 2 == 0)
+			_putchar(str[b]);
+	}
+
+	_putchar('\n');
+}
