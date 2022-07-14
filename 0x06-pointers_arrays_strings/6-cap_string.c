c7b39faeb750add2c9c005fe3069951574cc58f1
Khienna <saishat645@gmail.com>
2022-07-14 12:59:18 -0700

    task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * cap_string - to capitalize
+ * @str: string
+ * Return: string
+ */
+char *cap_string(char *str)
+{
+	char s[] = {',', ';', '.', '!', '?', '"', '(', ')', '{', '}', '\n', 32, 9};
+	int b;
+	int a = 0;
+	int c;
+	int d;
+
+	c = strlen(str);
+	while (a < c)
+	{
+		d = a + 2;
+		if (str[a] == s[b] && (str[d] >= 97 && str[d] <= 122))
+		{
+			str[d] = str[d] - 32;
+			b++;
+		}
+		a++;
+	}
+	return (str);
+}
