2ad89904e837974aab5d77805f7550aea0bbb483
Khienna <saishat645@gmail.com>
2022-07-18 09:31:20 -0700

    task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strchr - acts same way as strchr
+ * @s: string
+ * @c: character
+ * Return: c or null.
+ */
+char *_strchr(char *s, char c)
+{
+	strchr(s, c);
+	return (0);
+}
