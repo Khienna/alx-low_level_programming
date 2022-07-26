3c4e8288ba7bd7ef99ee27501b9e483c45474724
Khienna <saishat645@gmail.com>
2022-07-26 05:15:37 -0700

    Task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,9 +14,9 @@ char *str_concat(char *s1, char *s2)
 	int b;
 	int c;
 
-	if (s1 == 0)
+	if (s1 == 0 || s1 == NULL)
 		s1 = '\0';
-	if (s2 == 0)
+	if (s2 == 0 || s2 == NULL)
 		s2 = '\0';
 	for (a = 0; s1[a] != '\0'; a++)
 	{
