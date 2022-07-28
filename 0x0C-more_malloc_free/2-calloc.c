198ac26e8ff9b6c66024f163a25edffb29080d30
Khienna <saishat645@gmail.com>
2022-07-28 16:24:43 -0700

    TASK2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,18 +10,12 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *b;
-	char *c;
-	int d;
+	int *b;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	b = malloc(size * nmemb);
 	if (b == NULL)
 		return (NULL);
-	c = b;
-	for (d = 0; d < (size * nmemb); d++)
-		c[d] = '\0';
-
 	return (b);
 }
