a11490f84926a089ed20e5d8f7fae6125b601739
Khienna <saishat645@gmail.com>
2022-08-03 16:36:50 -0700

    Task3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+
+/**
+ * op_add - add
+ * @a: a
+ * @b: b
+ * Return: sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - substracts
+ * @a: a
+ * @b: b
+ * Return: subtract
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies
+ * @a: a
+ * @b: b
+ * Return: multiply
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides
+ * @a: a
+ * @b: b
+ * Return: division
+ */
+int op_div(int a, int b)
+{
+	return ( a / b);
+}
+
+/**
+ * op_mod - modulus
+ * @a: a
+ * @b: b
+ * Return: modulus
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
